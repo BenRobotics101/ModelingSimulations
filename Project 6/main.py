5638381f1122c2ebd4323b250e949c7ae46281f8
Benjamin Carter <59350660+BenRobotics101@users.noreply.github.com>
2023-11-29 21:42:17 -0700

    Fix initial conditions when x != 0

--- Project 6/main.py
+++ Project 6/main.py
@@ -19,22 +19,27 @@ def model2(x):
 
 
 x = np.linspace(0, 6, 100)  # range of x values
+x_adjust = np.linspace(6, 0, 100)  # range of x values
 y0 = [6.0, 1.0]
 
 # Solve the differential equation using odeint
 y_regular, y_prime = odeint(model, y0, x).T
+y_regular2, y_prime2 = odeint(model, y0, x_adjust).T
+
 
 taylor = model2(x)
     
 figure, axis = plt.subplots() 
 
-axis.plot(x, y_regular, label = "Real Solution", color = "green")
+axis.plot(x+3, y_regular, label = "Real Solution", color = "green")
+axis.plot(x_adjust-3, y_regular2, color = "green")
 axis.plot(x, taylor, label = "Second Order Taylor Polynomial", color = "red")
 axis.set_xlabel('x')
 axis.set_ylabel('y')
 
 axis.legend()
 axis.set_xlim(0, 6)
+axis.set_ylim(-20, 20)
 axis.set_title('Taylor Polynomial vs. Real Solution')
 
 plt.show()
